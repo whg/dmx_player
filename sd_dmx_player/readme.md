05a5a92cf475135bce0a31bfaf159ad51718f081
Will Gallia <will@stillatplay.com>
2019-05-23 19:43:46 +0100

    mods to player
    add:
    
    - trigger pin
    - num runs
    - timeout
    
    closes #1
    part way to #2

---
+++ sd_dmx_player/readme.md
@@ -0,0 +1,36 @@
+
+The top of the file looks like:
+
+```c++
+//#define DEBUG
+#define FILENAME "fog.raw"
+#define INITIAL_DELAY 60 // seconds
+#define TRIGGER_PIN A0
+#define NUM_RUNS 3
+#define TIMEOUT_SECONDS 3.2
+```
+
+These are the only things you should need to change.
+
+For the bottom 3, if you comment them out (or remove them), you will remove their functionality.
+
+So, with the following:
+
+```c++
+#define FILENAME "fog.raw"
+#define INITIAL_DELAY 60 // seconds
+```
+
+After 60 seconds, the player will loop `fog.raw` forever.
+x
+### TRIGGER_PIN
+
+The trigger pin is configured to be active low, i.e. it enables the pull up resistor. To use it, `#define TRIGGER_PIN` with some pin number.
+
+### NUM_RUNS
+
+If this is `#define`d then the player will stop after that number. If `TRIGGER_PIN` is defined then this it the player will be able to start again from beginning if it receives a trigger.
+
+### TIMEOUT_SECONDS
+
+This is related to `TRIGGER_PIN` and has no functionality unless it is also `#define`d. With this enabled, the program looks for changes on the `TRIGGER_PIN`, if there is no change within `TIMEOUT_SECONDS` the player is paused. A trigger will resume playback.
--- sd_dmx_player/sd_dmx_player.ino
+++ sd_dmx_player/sd_dmx_player.ino
@@ -4,39 +4,57 @@
 // framerate is limited to 255fps
 
 //#define DEBUG
+#define FILENAME "for.raw"
 #define INITIAL_DELAY 60 // seconds
+#define TRIGGER_PIN A0
+#define NUM_RUNS 3
+#define TIMEOUT_SECONDS 3.2
 
 #define DMX_CHANNELS 50
 #define DATA_START 6
+
+///////////////////////////////////////////////////
+
+enum state_t { STOPPED, PLAYING, PAUSED };
+state_t state = PLAYING;
 
 File dataFile;
 
 uint8_t buffer[DMX_CHANNELS];
 uint16_t numChannels;
 
-uint32_t numRows, frameCounter;
+uint32_t numRows, frameCounter, runCounter;
 uint32_t lastFrameTime;
 uint32_t frameDelay;
 
+#ifdef TIMEOUT_SECONDS
+uint32_t lastTriggerPinTime = 0;
+uint8_t lastTriggerPinState;
+#endif
+
+#define NOT_SET -1
+int triggerPin = NOT_SET;
+
 const uint8_t greenLed = A4, redLed = A5;
 
-class DMX {
-public:
-  static void init() {
-    // 2 stop bits, 8 data bits
-    UCSR0C = (1<<USBS0) | (1<<UCSZ00) | (1<<UCSZ01);
-  }
 
-  static void write(uint8_t *data, uint16_t len) {
-    Serial.begin(125000);
-    Serial.write(static_cast<uint8_t>(0));
-    Serial.flush();
-    
-    Serial.begin(250000);
-    Serial.write(static_cast<uint8_t>(0));
-    Serial.write(data, len);
-    Serial.flush();
-  }
+class DMX {
+  public:
+    static void init() {
+      // 2 stop bits, 8 data bits
+      UCSR0C = (1 << USBS0) | (1 << UCSZ00) | (1 << UCSZ01);
+    }
+
+    static void write(uint8_t *data, uint16_t len) {
+      Serial.begin(125000);
+      Serial.write(static_cast<uint8_t>(0));
+      Serial.flush();
+
+      Serial.begin(250000);
+      Serial.write(static_cast<uint8_t>(0));
+      Serial.write(data, len);
+      Serial.flush();
+    }
 };
 
 void reset() {
@@ -55,13 +73,17 @@ void setup() {
   pinMode(greenLed, OUTPUT);
   pinMode(redLed, OUTPUT);
 
+#ifdef TRIGGER_PIN
+  pinMode(triggerPin, INPUT_PULLUP);
+#endif
+
   digitalWrite(greenLed, HIGH);
   digitalWrite(redLed, HIGH);
-  
+
 
   if (!SD.begin(10)) {
 #ifdef DEBUG
-  Serial.println("SD fail");
+    Serial.println("SD fail");
 #endif
     //init failed
     while (true) {
@@ -94,7 +116,7 @@ void setup() {
 
     uint8_t framerate = dataFile.read();
     // intial versions of raw files had 23 has header
-    if (version == 7) { 
+    if (version == 7) {
       framerate = 44;
     }
 
@@ -107,7 +129,15 @@ void setup() {
     }
   }
 
+#ifdef TRIGGER_PIN
+  triggerPin = TRIGGER_PIN;
+  state = STOPPED;
+#endif
+
+
   reset();
+  runCounter = 0;
+
   while (millis() / 1000 < INITIAL_DELAY) {
     digitalWrite(greenLed, millis() % 1000 < 500);
   }
@@ -115,6 +145,34 @@ void setup() {
 }
 
 void loop() {
+#ifdef TRIGGER_PIN
+  uint8_t triggerState = digitalRead(triggerPin);
+  if (triggerState == LOW) {
+    if (state == STOPPED) {
+      reset();
+    }
+    state = PLAYING;
+  }
+
+#ifdef TIMEOUT_SECONDS
+  uint32_t now = millis();
+  if (triggerState != lastTriggerPinState) {
+    lastTriggerPinTime = now;
+    lastTriggerPinState = triggerState;
+  }
+
+  if (now - lastTriggerPinTime >= static_cast<uint32_t>(TIMEOUT_SECONDS * 1000)) {
+    state == PAUSED;
+  }
+#endif
+
+#endif
+
+
+  if (state != PLAYING) {
+    return;
+  }
+
   uint32_t start = millis();
 
   dataFile.read(buffer, numChannels);
@@ -124,7 +182,6 @@ void loop() {
   Serial.print(buffer[0]);
   Serial.print(",");
   Serial.println(buffer[1]);
-
 #else
   DMX::write(buffer, numChannels);
 #endif
@@ -132,6 +189,14 @@ void loop() {
 
   if (frameCounter++ >= numRows) {
     reset();
+
+#ifdef NUM_RUNS
+    if (++runCounter >= NUM_RUNS) {
+      state = STOPPED;
+    }
+#endif
+
+
 #ifdef DEBUG
     Serial.println("---");
 #endif
